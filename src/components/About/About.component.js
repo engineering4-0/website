abc00a1e685ac8349797b53cf2ad9ddfc2f98ba8
Raj Kumar <rajkumar.cind@gmail.com>
2023-02-01 13:12:21 -0500

    updated styling

--- src/components/About/About.component.js
+++ src/components/About/About.component.js
@@ -9,11 +9,8 @@ const About = () => {
             <Grid style={{ marginBottom: "6em" }}>
                 <Column lg={1} md={0} sm={0}></Column>
                 <Column lg={7} md={4} sm={4} className='department-section'>
-                    <p className='departments hover-underline-animation'>Mechanical</p><br></br>
-                    <p className='departments hover-underline-animation'>Computer Science</p><br></br>
-                    <p className='departments hover-underline-animation'>Electrical</p><br></br>
-                    <p className='departments hover-underline-animation'>people intrested in engineering..</p><br></br>
-                    <p className='departments hover-underline-animation'>The club is for every engineer!</p>
+                    <p className='departments hover-underline-animation'>People intrested in engineering</p><br></br>
+                    <p className='departments hover-underline-animation'>The club is for every engineer and tech enthusiasts!</p>
                 </Column>
                 <Column lg={7} md={4} sm={4}>
                     <p style={{ fontWeight: 400, fontSize: "20px", marginBottom: "1em" }}>We are a diverse, inclusive student-run organization seeking
@@ -21,19 +18,19 @@ const About = () => {
                     <div>
                         <hr></hr>
                         <UnorderedList >
-                            <ListItem style={{ color: "#616161" }}>
+                            <ListItem style={{ fontSize: "17px", fontWeight: 600 }}>
                                 Attend workshops and talks by industry professionals
                             </ListItem>
-                            <ListItem style={{ color: "#616161" }}>
+                            <ListItem style={{ fontSize: "17px", fontWeight: 600 }}>
                                 Work on real-world projects,
                             </ListItem>
-                            <ListItem style={{ color: "#616161" }}>
+                            <ListItem style={{ fontSize: "17px", fontWeight: 600 }}>
                                 Organize study groups,
                             </ListItem>
-                            <ListItem style={{ color: "#616161" }}>
+                            <ListItem style={{ fontSize: "17px", fontWeight: 600 }}>
                                 Get peer review or help to participate in technical competitions and paper presentations, (not to forget!)
                             </ListItem>
-                            <ListItem style={{ color: "#616161" }}>
+                            <ListItem style={{ fontSize: "17px", fontWeight: 600 }}>
                                 Grow your professional network through social events, and so much more!
                             </ListItem>
                         </UnorderedList>
--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -8,49 +8,15 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
 
-
     return (
         <div style={{ marginBottom: "6em" }}>
             <HeaderContainer
                 render={({ isSideNavExpanded, onClickSideNavExpand }) => (
                     <CarbonHeader aria-label="Carbon Tutorial">
                         <SkipToContent />
-                        {/* <HeaderMenuButton
-                            aria-label="Open menu"
-                            onClick={onClickSideNavExpand}
-                            isActive={isSideNavExpanded}
-                        /> */}
                         <HeaderName element={Link} to="/" prefix="" style={{ fontSize: "20px" }}>
                             Engineering 4.0| UWindsor
                         </HeaderName>
-                        {/* <HeaderNavigation aria-label="Carbon Tutorial"> */}
-                            {/* <HeaderMenuItem
-                                isCurrentPage={location.pathname === '/register'}
-                                element={Link}
-                                to="/register"
-                            >
-                                Register
-                            </HeaderMenuItem> */}
-                            {/* <HeaderMenuItem
-                                isCurrentPage={location.pathname === '/about'}
-                                element={Link}
-                                to="/about"
-                            >
-                                About
-                            </HeaderMenuItem>
-                        </HeaderNavigation> */}
-                        {/* <SideNav
-                            aria-label="Side navigation"
-                            isPersistent={false}
-                            expanded={isSideNavExpanded}
-                        >
-                            <SideNavItems>
-                                <HeaderSideNavItems>
-                                    <HeaderMenuItem element={Link} to="/register">Register</HeaderMenuItem>
-                                    <HeaderMenuItem element={Link} to="/about">About</HeaderMenuItem>
-                                </HeaderSideNavItems>
-                            </SideNavItems>
-                        </SideNav> */}
                     </CarbonHeader>
                 )}
             />
