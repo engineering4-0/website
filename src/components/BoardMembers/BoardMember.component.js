8d6d6a46ebe842050a4b9dcfaf742f2f0dc79d69
Raj Kumar <rajkumar.cind@gmail.com>
2023-02-12 11:46:14 -0500

    updated the position of irene

--- src/components/BoardMembers/BoardMember.component.js
+++ src/components/BoardMembers/BoardMember.component.js
@@ -56,7 +56,7 @@ const BoardMembers = () => {
                         </Column>
                         <Column lg={2} md={4} sm={2}>
                             Irene<hr></hr>
-                            Social Media Head
+                            Head of Administration
                         </Column>
                     </Grid>
                 </ClickableTile>
