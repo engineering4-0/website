854fd59336550c81a6af4792815e05da902b6e97
Raj Kumar <rajkumar.cind@gmail.com>
2023-03-08 15:01:24 -0500

    added career options

--- src/components/Career/Career.component.js
+++ src/components/Career/Career.component.js
@@ -2,6 +2,8 @@ import {
     ExpandableTile,
     TileAboveTheFoldContent,
     TileBelowTheFoldContent,
+    UnorderedList,
+    ListItem,
 } from '@carbon/react';
 
 const Career = () => {
@@ -22,12 +24,93 @@ const Career = () => {
                 </TileAboveTheFoldContent>
                 <TileBelowTheFoldContent>
                     <div style={{ marginLeft: "2em" }}>
-                        <p style={{ fontWeight: 600 }}>Role: Web Developer</p>
+                        <p style={{ fontWeight: 600 }}>Role: Web Developer</p><br></br>
                         <p>Responsibilities:</p>
-                        
+                        <UnorderedList>
+                            <ListItem style={{ color: "#616161" }}>
+                                Basic knowledge of React (Optional)
+                            </ListItem>
+                            <ListItem style={{ color: "#616161" }}>
+                                Have an understading of HTML, CSS and Javascript
+                            </ListItem>
+                            <ListItem style={{ color: "#616161" }}>
+                                Basic knowledge of GitHub
+                            </ListItem>
+                            <ListItem style={{ color: "#616161" }}>
+                                Having a great sense of humour is always a plus (Optional)
+                            </ListItem>
+                        </UnorderedList>
                     </div>
                 </TileBelowTheFoldContent>
             </ExpandableTile>
+
+            {/* <ExpandableTile style={{ marginBottom: "2em" }}
+                tabIndex={0}
+                tileCollapsedIconText="Interact to Expand tile"
+                tileExpandedIconText="Interact to Collapse tile"
+                tileMaxHeight={0}
+                tilePadding={0}
+                light={false} expanded={false}>
+                <TileAboveTheFoldContent>
+                    <div style={{ height: '64px', fontSize: "1.5em" }}>
+                        Treasurer Team
+                    </div>
+                </TileAboveTheFoldContent>
+                <TileBelowTheFoldContent>
+                    <div style={{ marginLeft: "2em" }}>
+                        <p style={{ fontWeight: 600 }}>Role: Data Handler</p><br></br>
+                        <p>Responsibilities:</p>
+                        <UnorderedList>
+                            <ListItem style={{ color: "#616161" }}>
+                                Basic knowledge of React (Optional)
+                            </ListItem>
+                            <ListItem style={{ color: "#616161" }}>
+                                Have an understading of HTML, CSS and Javascript
+                            </ListItem>
+                            <ListItem style={{ color: "#616161" }}>
+                                Basic knowledge of GitHub
+                            </ListItem>
+                            <ListItem style={{ color: "#616161" }}>
+                                Having a great sense of humour is always a plus (Optional)
+                            </ListItem>
+                        </UnorderedList>
+                    </div>
+                </TileBelowTheFoldContent>
+            </ExpandableTile>
+
+            <ExpandableTile style={{ marginBottom: "2em" }}
+                tabIndex={0}
+                tileCollapsedIconText="Interact to Expand tile"
+                tileExpandedIconText="Interact to Collapse tile"
+                tileMaxHeight={0}
+                tilePadding={0}
+                light={false} expanded={false}>
+                <TileAboveTheFoldContent>
+                    <div style={{ height: '64px', fontSize: "1.5em" }}>
+                        Operational Team
+                    </div>
+                </TileAboveTheFoldContent>
+                <TileBelowTheFoldContent>
+                    <div style={{ marginLeft: "2em" }}>
+                        <p style={{ fontWeight: 600 }}>Role: Data Handler</p><br></br>
+                        <p>Responsibilities:</p>
+                        <UnorderedList>
+                            <ListItem style={{ color: "#616161" }}>
+                                Basic knowledge of React (Optional)
+                            </ListItem>
+                            <ListItem style={{ color: "#616161" }}>
+                                Have an understading of HTML, CSS and Javascript
+                            </ListItem>
+                            <ListItem style={{ color: "#616161" }}>
+                                Basic knowledge of GitHub
+                            </ListItem>
+                            <ListItem style={{ color: "#616161" }}>
+                                Having a great sense of humour is always a plus (Optional)
+                            </ListItem>
+                        </UnorderedList>
+                    </div>
+                </TileBelowTheFoldContent>
+            </ExpandableTile> */}
         </>
     );
 };
--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -10,6 +10,7 @@ import Fade from 'react-reveal/Fade';
 import Registration from '../../components/Registration/Registration.component';
 import { useNavigate } from 'react-router-dom';
 import BoardMembers from '../../components/BoardMembers/BoardMember.component';
+import Career from '../../components/Career/Career.component';
 
 const HomePage = () => {
 
@@ -27,59 +28,18 @@ const HomePage = () => {
             </Column>
             <Column lg={1} md={0} sm={0}></Column>
         </Grid>
-        {/* <Fade bottom>
-            <Grid style={{ marginBottom: "6em" }}>
-                <Column lg={1} md={0} sm={0}></Column>
-
-                <Column lg={7} md={4} sm={4} className='department-section'>
-                    <p className='departments hover-underline-animation'>Mechanical</p><br></br>
-                    <p className='departments hover-underline-animation'>Computer Science</p><br></br>
-                    <p className='departments hover-underline-animation'>Electrical</p><br></br>
-                    <p className='departments hover-underline-animation'>people intrested in engineering..</p><br></br>
-                    <p className='departments hover-underline-animation'>The club is for every engineer!</p>
-                </Column>
-
-                <Column lg={7} md={4} sm={4}>
-                    <p style={{ fontWeight: 400, fontSize: "20px", marginBottom: "1em" }}>We are a diverse, inclusive student-run organization seeking
-                        to provide a platform to the science and technology enthusiasts of University of Windsor to</p>
-                    <div>
-                        <hr></hr>
-                        <UnorderedList >
-                            <ListItem style={{ color: "#616161" }}>
-                                Attend workshops and talks by industry professionals
-                            </ListItem>
-                            <ListItem style={{ color: "#616161" }}>
-                                Work on real-world projects,
-                            </ListItem>
-                            <ListItem style={{ color: "#616161" }}>
-                                Organize study groups,
-                            </ListItem>
-                            <ListItem style={{ color: "#616161" }}>
-                                Get peer review or help to participate in technical competitions and paper presentations, (not to forget!)
-                            </ListItem>
-                            <ListItem style={{ color: "#616161" }}>
-                                Grow your professional network through social events, and so much more!
-                            </ListItem>
-                        </UnorderedList>
-                    </div>
-                </Column>
-                <Column lg={1} md={0} sm={0}></Column>
-
-            </Grid>
-        </Fade> */}
-        {/* <Fade botton>
-        
+        <Fade botton>
             <Grid style={{ marginBottom: "3em" }}>
                 <Column lg={2} md={0} sm={0}></Column>
                 <Column lg={12} md={8} sm={4} >
-                <p className='heading-02'>Careers</p><hr />
-                <p className='sub-heading'>Apply to work with us</p><br />
-                <p style={{ color: "white" ,marginBottom: "1em" }}>Send us an email with your resume to engineer4-0@outlook.com with the role as the subject.</p>
+                    <p className='heading-02'>Careers</p><hr />
+                    <p className='sub-heading'>Work with an awesome club!</p><br />
+                    <p style={{ color: "white", marginBottom: "1em" }}>Send us an email with your resume to engineer4-0@outlook.com with the role as the subject.</p>
                     <Career />
                 </Column>
                 <Column lg={2} md={0} sm={0}></Column>
             </Grid>
-        </Fade> */}
+        </Fade>
         <Fade bottom>
             <BoardMembers />
         </Fade>
