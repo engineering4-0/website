564ca5b6a9989d0245270851a4078e2429736ab5
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2023-09-29 01:53:45 -0400

    header changes

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -10,6 +10,7 @@ import {
   SideNavItems,
   HeaderMenuButton,
 } from "@carbon/react";
+import { Column, Button } from "@carbon/react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import image from "./icon-180x180.png";
 import "./Header.style.css";
@@ -21,59 +22,84 @@ const Header = () => {
   return (
     <div>
       <HeaderContainer
+        className="Hello_there"
         render={({ isSideNavExpanded, onClickSideNavExpand }) => (
-          <CarbonHeader aria-label="Carbon Tutorial">
+          <CarbonHeader aria-label="Carbon Tutorial" className="carbon_navbar">
             <SkipToContent />
             <HeaderMenuButton
-              aria-label={isSideNavExpanded ? 'Close menu' : 'Open menu'}
+              aria-label={isSideNavExpanded ? "Close menu" : "Open menu"}
               onClick={onClickSideNavExpand}
               isActive={isSideNavExpanded}
             />
-            <HeaderName
-              element={Link}
-              to="/"
-              prefix=""
-              className="nav-header"
-            >
+            <HeaderName element={Link} to="/" prefix="" className="nav-header">
               Engineering 4.0{" "}
               <span className="nav-sub-heading">| UWindsor</span>{" "}
-              <img
-                src={image}
-                className="header-logo"
-                alt="logo"
-              />
+              <img src={image} className="header-logo" alt="logo" />
             </HeaderName>
             <HeaderNavigation aria-label="Upcoming Events">
               <HeaderMenuItem
                 style={{ cursor: "pointer" }}
-                onClick={() => navigate("/events")} 
-                isCurrentPage={ (location.pathname === '/events') ? true : false }>
+                onClick={() => navigate("/events")}
+                isCurrentPage={location.pathname === "/events" ? true : false}
+              >
                 Events
               </HeaderMenuItem>
               <HeaderMenuItem
                 style={{ cursor: "pointer" }}
-                onClick={() => navigate("/buddy")} 
-                isCurrentPage={(location.pathname === '/buddy') ? true : false }>
+                onClick={() => navigate("/buddy")}
+                isCurrentPage={location.pathname === "/buddy" ? true : false}
+              >
                 Buddy Program 🌟
+              </HeaderMenuItem>
+              <HeaderMenuItem>
+                <Column
+                  lg={5}
+                  md={8}
+                  sm={4}
+                  style={{ marginTop: "1em", marginBottom: "1em" }}
+                >
+                  <Button kind="primary" className="member_button">
+                    Become a member
+                  </Button>
+                </Column>
               </HeaderMenuItem>
             </HeaderNavigation>
             <SideNav
               aria-label="Side navigation"
               expanded={isSideNavExpanded}
-              isPersistent={false}>
+              isPersistent={false}
+            >
               <SideNavItems>
                 <HeaderSideNavItems>
                   <HeaderMenuItem
                     style={{ cursor: "pointer" }}
                     onClick={() => navigate("/events")}
-                    isCurrentPage={ (location.pathname === '/events') ? true : false }>
+                    isCurrentPage={
+                      location.pathname === "/events" ? true : false
+                    }
+                  >
                     Events
                   </HeaderMenuItem>
                   <HeaderMenuItem
                     style={{ cursor: "pointer" }}
                     onClick={() => navigate("/buddy")}
-                    isCurrentPage={(location.pathname === '/buddy') ? true : false }>
+                    isCurrentPage={
+                      location.pathname === "/buddy" ? true : false
+                    }
+                  >
                     Buddy Program 🌟
+                  </HeaderMenuItem>
+                  <HeaderMenuItem>
+                    <Column
+                      lg={5}
+                      md={8}
+                      sm={4}
+                      style={{ marginTop: "1em", marginBottom: "1em" }}
+                    >
+                      <Button kind="primary" className="member_button">
+                        Become a member
+                      </Button>
+                    </Column>
                   </HeaderMenuItem>
                 </HeaderSideNavItems>
               </SideNavItems>
