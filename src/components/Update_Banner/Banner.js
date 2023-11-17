f1fdd787d629863746d2fbbf6ae3eae9febb3063
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2023-11-16 19:55:11 -0500

    Fixed Instagram link

--- src/components/Update_Banner/Banner.js
+++ src/components/Update_Banner/Banner.js
@@ -1,41 +1,43 @@
-import React from "react";
-import { FaDiscord } from "react-icons/fa";
-import { AiOutlineInstagram, AiFillLinkedin } from "react-icons/ai";
-import "./banner.style.css";
+import React from 'react';
+import { FaDiscord } from 'react-icons/fa';
+import { AiOutlineInstagram, AiFillLinkedin } from 'react-icons/ai';
+import './banner.style.css';
 
 const Banner = (props) => {
   const { message } = props;
   return (
     <div className="Banner">
-      {message}{" "}
-      {message === "Follow our social media platform for regular updates" ? (
+      {message}{' '}
+      {message === 'Follow our social media platform for regular updates' ? (
         <span>
-          <AiOutlineInstagram
-            size="2em"
-            fill="black"
-            className="icons instagram"
-            onClick={() =>
-              (window.location.href = "https://www.instagram.com/eng4.0_uwin/")
-            }
-          />
-          <AiFillLinkedin
-            size="2em"
-            fill="black"
-            className="icons"
-            onClick={() =>
-              (window.location =
-                "https://www.linkedin.com/company/engineering-4-0/")
-            }
-          />
-          <FaDiscord
-            size="2em"
-            fill="black"
-            className="icons"
-            onClick={() => (window.location = "https://discord.gg/Z8JnPSC5tD")}
-          />
+          <a
+            href="https://www.instagram.com/uwindsor_eng4.0/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineInstagram
+              size="2em"
+              fill="black"
+              className="icons instagram"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/engineering-4-0/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiFillLinkedin size="2em" fill="black" className="icons" />
+          </a>
+          <a
+            href="https://discord.gg/Z8JnPSC5tD"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaDiscord size="2em" fill="black" className="icons" />
+          </a>
         </span>
       ) : (
-        ""
+        ''
       )}
     </div>
   );
