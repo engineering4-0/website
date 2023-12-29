99b3a467f598e37cc296c61f75c2e174baf82463
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2023-12-29 15:00:30 -0500

    added caption tag

--- src/components/Home/HeroPhotos.jsx
+++ src/components/Home/HeroPhotos.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 const HeroPhotos = (props) => {
   return (
     <div
-      className={`${props.className} hidden lg:flex rounded-lg shadow-2xl shadow-gray-400  p-2 justify-self-center flex-col items-center overflow-clip`}
+      className={`${props.className} hidden lg:flex rounded-lg shadow-2xl shadow-gray-600  p-2 justify-self-center flex-col items-center overflow-clip`}
     >
       <img
         src={props.img}
         alt=""
         className="max-w-48 w-full bg-black aspect-square object-cover"
       />
-      <p className="max-w-40 text-center text-xs my-1">
-        Lorem ipsum dolor sit amet.
-      </p>
+      <caption className="max-w-40 text-center text-xs my-1">
+        {props.caption ?? 'Engineering club 4.0 event'}
+      </caption>
     </div>
   );
 };
