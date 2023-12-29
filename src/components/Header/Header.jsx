756f3f111537e4011d47365a5c58d881a686b808
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2023-12-29 18:35:19 -0500

    header scroll animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 import logo from './icon-180x180.png';
 
 const Header = () => {
+  const [isNavScroll, setIsNavScroll] = useState(false);
+  const headerRef = useRef();
+
+  useEffect(() => {
+    if (!headerRef.current) return;
+
+    const changeNav = () => {
+      if (window.scrollY >= headerRef.current.clientHeight) {
+        setIsNavScroll(true);
+      } else {
+        setIsNavScroll(false);
+      }
+    };
+
+    window.addEventListener('scroll', changeNav);
+
+    return () => window.removeEventListener('scroll', changeNav);
+  }, [headerRef]);
+
   return (
-    <header className="flex justify-between items-center px-8 pt-6 pb-2.5 sticky top-0 z-50">
-      <Link to="/" className="flex items-center gap-2">
+    <header
+      className="flex justify-between items-center px-8 pt-6 pb-2.5 sticky top-0 z-50"
+      ref={headerRef}
+    >
+      <Link
+        to="/"
+        className={`flex items-center gap-2 ${
+          isNavScroll && 'opacity-0 cursor-default'
+        } transition-opacity duration-300 ease-in-out`}
+        onClick={(e) => {
+          if (isNavScroll) e.preventDefault();
+        }}
+        aria-disabled={isNavScroll}
+      >
         <img src={logo} alt="Engineering Club 4.0 logo" className="w-16" />
-        <caption className="font-bold text-2xl text-accent-900 tracking-wide">
-          Engineering Club 4.0
-        </caption>
+        <div className="font-bold text-2xl text-accent-900 tracking-wide">
+          Engineering 4.0
+          <span className="font-normal border-accent-900 border-l-2 mx-2"></span>
+          <span className="font-thin">UWindsor</span>
+        </div>
       </Link>
 
-      <nav>
-        <ul className="flex gap-6">
+      <nav
+        className={`${
+          isNavScroll &&
+          'shadow-2xl shadow-slate-400 rounded-lg  justify-around gap-0 backdrop-blur-2xl bg-[#ffffff80]'
+        } transition-all duration-300 ease-in-out self-center p-4`}
+      >
+        <ul className="flex gap-8 justify-evenly">
           <li>
             <NavLink to="/">Home</NavLink>
           </li>
