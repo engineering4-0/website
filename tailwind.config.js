d1dfe293f90517839ceb272ee901dd4451ef1ee7
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2023-12-29 00:29:04 -0500

    Installed tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        accent: colors.blue,
+      },
+    },
+  },
+  plugins: [],
+};
