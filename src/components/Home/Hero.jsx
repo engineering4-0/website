48c7d0b76b7d31a74aaff8e4a5f9960834dc077c
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2023-12-29 15:51:45 -0500

    changed max width of text and updated the link

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -12,11 +12,11 @@ const Hero = () => {
     <div className="h-svh grid lg:grid-cols-5 lg:grid-rows-2 place-items-center px-10 overflow-hidden">
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
       <div className="container row-span-2 col-start-2 col-end-5 col-span-3 flex flex-col items-center justify-center gap-10 mx-auto py-10">
-        <div className="text-center max-w-2xl 2xl:max-w-3xl">
+        <div className="text-center max-w-2xl 2xl:max-w-3xl flex flex-col items-center">
           <h1 className="font-bold uppercase mb-4 text-accent-900 text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl">
             Student-run, Applied Science Club!
           </h1>
-          <p>
+          <p className="max-w-2xl">
             Engineering 4.0 is a student-run organization dedicated to
             supporting and providing a platform for all engineers and tech
             enthusiasts of the{' '}
@@ -28,7 +28,12 @@ const Hero = () => {
           <a href="/" className="btn-accent">
             Join us
           </a>
-          <a href="https://linktr.ee/uwindsor_eng4.0" className="btn-primary">
+          <a
+            href="https://linktr.ee/uwindsor_eng4.0"
+            target="_blank"
+            rel="noreferrer"
+            className="btn-primary"
+          >
             Learn More
           </a>
         </div>
