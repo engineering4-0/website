49d3c6c952d1da15521f87d4a71df05185c05a01
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2023-12-30 13:54:18 -0500

    added rounded corners to the pics

--- src/components/Home/HeroPhotos.jsx
+++ src/components/Home/HeroPhotos.jsx
@@ -5,7 +5,7 @@ const HeroPhotos = (props) => {
     <div
       className={`${props.className} hidden flex-col items-center justify-self-center overflow-clip rounded-lg bg-white p-2 shadow-2xl shadow-gray-600 lg:flex`}
     >
-      <img src={props.img} alt="" className="aspect-square w-full max-w-48 object-cover " />
+      <img src={props.img} alt="" className="aspect-square w-full max-w-48 rounded-sm object-cover" />
       <caption className="my-1 max-w-40 text-center text-xs">{props.caption ?? "Engineering club 4.0 event"}</caption>
     </div>
   );
