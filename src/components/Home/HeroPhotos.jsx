9d08175a82f3bb4bafd12c61a649c9e2b67f6ae7
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2023-12-31 16:29:16 -0500

    changed caption tag to div

--- src/components/Home/HeroPhotos.jsx
+++ src/components/Home/HeroPhotos.jsx
@@ -6,7 +6,7 @@ const HeroPhotos = (props) => {
       className={`${props.className} hidden flex-col items-center justify-self-center overflow-clip rounded-lg bg-white p-2 shadow-2xl shadow-gray-600 lg:flex`}
     >
       <img src={props.img} alt="" className="aspect-square w-full max-w-48 rounded-sm object-cover" />
-      <caption className="my-1 max-w-40 text-center text-xs">{props.caption ?? "Engineering club 4.0 event"}</caption>
+      <div className="my-1 max-w-40 text-center text-xs">{props.caption ?? "Engineering club 4.0 event"}</div>
     </div>
   );
 };
