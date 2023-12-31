979803835e78b7449f59dcc7d7eec3622127f9d4
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2023-12-31 16:28:55 -0500

    moved footer to app.js

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,13 +1,21 @@
 import Hero from "../../components/Home/Hero";
 import BoardMembers from "../../components/BoardMembers/BoardMembers";
-import Footer from "../../components/Footer/Footer";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <BoardMembers />
-      <Footer />
+      <div className="mb-14 mt-8 flex flex-col items-center">
+        <BoardMembers />
+        <Link to="/members" className="font-bold text-accent-600 hover:text-accent-500">
+          <span className="hover:underline">
+            View all our members
+            <FaArrowRightLong className="ml-2 inline" />
+          </span>
+        </Link>
+      </div>
     </>
   );
 };
