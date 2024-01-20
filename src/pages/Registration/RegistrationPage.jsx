fdd6105d36bbd5a21d78d8b11692482bce9ef23f
Karthi Krishnamurthi <karthi13.murthi@gmail.com>
2024-01-19 22:42:48 -0500

    deleted styles page

--- src/pages/Registration/RegistrationPage.jsx
+++ src/pages/Registration/RegistrationPage.jsx
@@ -1,23 +1,9 @@
-import { Grid, Column } from "@carbon/react";
-import React from "react";
-import About from "../../components/About/About";
 import Registration from "../../components/Registration/Registration";
-import "./Registration.style.css";
 
 const RegistrationPage = () => {
   return (
-    <div className="registration-container">
-      <Grid>
-        <Column lg={1} md={0} sm={0}></Column>
-        <Column lg={6} md={4} sm={0}>
-          <About />
-        </Column>
-        <Column lg={1} md={0} sm={0}></Column>
-        <Column lg={5} md={12} sm={4}>
-          <Registration />
-        </Column>
-        <Column lg={1} md={0} sm={0}></Column>
-      </Grid>
+    <div>
+      <Registration />
     </div>
   );
 };
